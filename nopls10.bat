f68ba3cafb32c2b60e477955af7a7f3d3d666a12
Marion <marion@localhost.git>
2018-12-09 23:44:48 +0100

    Initial Commit

---
+++ nopls10.bat
@@ -0,0 +1,30 @@
+takeown /f C:\Windows\System32\GWX /r
+icacls C:\Windows\System32\GWX\* /t  /grant *S-1-1-0:F
+icacls C:\Windows\System32\GWX /t  /grant *S-1-1-0:F
+taskkill /f /im GWX.exe
+taskkill /f /im GWXUX.exe
+rmdir /Q /S "C:\Windows\System32\GWX"
+
+takeown /f C:\Windows\System32\GWX /r
+icacls C:\Windows\System32\GWX\* /t  /grant *S-1-1-0:F
+icacls C:\Windows\System32\GWX /t  /grant *S-1-1-0:F
+taskkill /f /im GWX.exe
+taskkill /f /im GWXUX.exe
+rmdir /Q /S "C:\Windows\System32\GWX"
+
+
+takeown /f C:\Windows\System32\GWX /r
+icacls C:\Windows\System32\GWX\* /t  /grant *S-1-1-0:F
+icacls C:\Windows\System32\GWX /t  /grant *S-1-1-0:F
+taskkill /f /im GWX.exe
+taskkill /f /im GWXUX.exe
+rmdir /Q /S "C:\Windows\System32\GWX"
+
+
+takeown /f C:\Windows\System32\GWX /r
+icacls C:\Windows\System32\GWX\* /t  /grant *S-1-1-0:F
+icacls C:\Windows\System32\GWX /t  /grant *S-1-1-0:F
+taskkill /f /im GWX.exe
+taskkill /f /im GWXUX.exe
+rmdir /Q /S "C:\Windows\System32\GWX"
+
